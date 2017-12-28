d9a42eca95f039107d16ce550ce658d8f4efe652
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2017-12-28 20:02:51 +0100

    example: drop semicolons from worker script

--- example/worker.js
+++ example/worker.js
@@ -1,9 +1,9 @@
-importScripts('filter.js');
+importScripts('filter.js')
 
-const filter = wobble(90);
+const filter = wobble(100)
 
 self.addEventListener('message', (e) => {
   self.postMessage({
     result: filter(e.data.source)
-  });
-});
+  })
+})
