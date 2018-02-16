fd90182497b77c5eaac8c30494dadf2af40bf082
Sotiris Bakagiannis <sotirios.bakagiannis@gmail.com>
2018-02-16 08:53:08 +0100

    module: rebuild cjs version sans the buble

--- index.js
+++ index.js
@@ -5,24 +5,20 @@
 
 // Set up with number of strips (resolution),
 // get a lambda for processing each data frame in return
-var wobble = function (depth) {
-  if ( depth === void 0 ) depth = 40;
-
+const wobble = (depth = 40) => {
   // For caching consecutive frames
-  var store = [];
+  const store = [];
 
   // Accepts and returns an `ImageData` like object, of which
   // `data` of type `Uint8ClampedArray` is the only required property
-  return function (input) {
-    if ( input === void 0 ) input = { data: [] };
-
+  return (input = { data: [] }) => {
     // Wrap input just in case, this is the data view that
     // gets processed in place
-    var frame = new Uint8ClampedArray(input.data.buffer);
+    const frame = new Uint8ClampedArray(input.data.buffer);
 
     // Copy input data, save for later
-    var clone = new Uint8ClampedArray(frame);
-    var storeSizeMaybe = store.push(clone);
+    const clone = new Uint8ClampedArray(frame);
+    const storeSizeMaybe = store.push(clone);
 
     // Limit store size within resolution
     if (depth - storeSizeMaybe < 0) {
@@ -30,21 +26,21 @@ var wobble = function (depth) {
     }
 
     // Calculate range in pixels for each strip
-    var storeSize = store.length;
-    var frameSize = frame.length;
+    const storeSize = store.length;
+    const frameSize = frame.length;
 
-    var chunkSize = Math.floor(frameSize / storeSize);
+    const chunkSize = Math.floor(frameSize / storeSize);
 
     // Avoid using forEach, because speed matters in this case
-    for (var i = 0; i < storeSize; i += 1) {
+    for (let i = 0; i < storeSize; i += 1) {
       // Chunk start
-      var a = i * chunkSize;
+      const a = i * chunkSize;
 
       // Chunk end
-      var b = a + chunkSize;
+      const b = a + chunkSize;
 
-      var block = store[i];
-      var chunk = block.subarray(a, b);
+      const block = store[i];
+      const chunk = block.subarray(a, b);
 
       frame.set(chunk, a);
     }
@@ -54,4 +50,3 @@ var wobble = function (depth) {
 };
 
 module.exports = wobble;
-
