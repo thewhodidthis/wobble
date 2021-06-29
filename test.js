cc812e2301565515bad47695e3ca3bed2d53a557
Sotiri Bakagiannis <sotirios.bakagiannis@gmail.com>
2021-06-29 22:01:37 +0100

    test: use imports, double quotes

--- test.js
+++ test.js
@@ -1,20 +1,21 @@
-'use strict'
+import { assert, report } from "tapeless"
+import createFilter from "./main.js"
 
-const { equal } = require('tapeless')
-const createFilter = require('./')
-
+const { equal } = assert
 const filter = createFilter()
 const { data } = filter()
 
 equal
-  .describe('returns lambda on init')
-  .test(typeof filter, 'function')
-  .describe('no data', 'will default')
+  .describe("returns lambda on init")
+  .test(typeof filter, "function")
+  .describe("no data", "will default")
   .test(data.length, 0)
 
 const source = { data: [1, 2, 3, 4] }
 const result = filter(source)
 
 equal
-  .describe('input/output size is a match', 'will operate')
+  .describe("input/output size is a match", "will operate")
   .test(result.data.length, source.data.length)
+
+report()
